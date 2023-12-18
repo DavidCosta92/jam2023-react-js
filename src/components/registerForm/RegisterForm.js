6638975553078c48916fe69beb17d3ef848abb00
Mathias Ledesma <romanmathias21@gmail.com>
2023-12-17 21:26:02 -0300

    DNI and email fields in register form was added

--- src/components/registerForm/RegisterForm.js
+++ src/components/registerForm/RegisterForm.js
@@ -47,6 +47,11 @@ export default function RegisterForm(){
                 <div id="email" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp"/>
+                <div id="email" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
                 <input type="password" class="form-control" id="password"/>
             </div>
@@ -62,6 +67,10 @@ export default function RegisterForm(){
             <div class="mb-3">
                 <label for="lastName" class="form-label">Apellido</label>
                 <input type="text" class="form-control" id="lastName"/>
+            </div>
+            <div class="mb-3">
+                <label for="lastName" class="form-label">DNI</label>
+                <input type="number" class="form-control" id="dni"/>
             </div>
             <div class="mb-3">
                 <label for="country" class="form-label">Pais</label>
